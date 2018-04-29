ce6c4721af6d3276da5831e7776228fe7fe018f3
Blargerist <yaywarcraft@gmail.com>
2018-04-29 12:45:27 -0600

    Add option to draw direct lines for advancement connections Add options for advancement connection line color when completed/uncompleted

--- src/main/java/betteradvancements/config/ConfigHandler.java
+++ src/main/java/betteradvancements/config/ConfigHandler.java
@@ -42,6 +42,10 @@ public class ConfigHandler {
 
         GuiBetterAdvancementTab.doFade = config.get(Configuration.CATEGORY_GENERAL, "doAdvancementsBackgroundFade", true).getBoolean();
 
+        BetterDisplayInfo.defaultDrawDirectLines = config.get(Configuration.CATEGORY_GENERAL, "defaultDrawDirectLines", false).getBoolean();
+        BetterDisplayInfo.defaultCompletedLineColor = ColorHelper.RGB(config.get(Configuration.CATEGORY_GENERAL, "defaultCompletedLineColor", "#FFFFFF").getString());
+        BetterDisplayInfo.defaultUncompletedLineColor = ColorHelper.RGB(config.get(Configuration.CATEGORY_GENERAL, "defaultUncompletedLineColor", "#FFFFFF").getString());
+
         if (config.hasChanged()) {
             config.save();
         }
--- src/main/java/betteradvancements/util/RenderUtil.java
+++ src/main/java/betteradvancements/util/RenderUtil.java
@@ -1,7 +1,12 @@
 package betteradvancements.util;
 
+import org.lwjgl.opengl.GL11;
+
 import net.minecraft.client.gui.Gui;
+import net.minecraft.client.renderer.BufferBuilder;
 import net.minecraft.client.renderer.GlStateManager;
+import net.minecraft.client.renderer.Tessellator;
+import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
 
 public class RenderUtil {
     private RenderUtil() {}
@@ -21,5 +26,23 @@ public class RenderUtil {
 
     public static void setColor(int color) {
         GlStateManager.color(((color >> 16) & 255) / 255F, ((color >> 8) & 255) / 255F, (color & 255) / 255F);
+    }
+    
+    public static void drawLineStrip(double x, double y, double x2, double y2, float width, int color)
+    {
+        Tessellator tessellator = Tessellator.getInstance();
+        BufferBuilder bufferbuilder = tessellator.getBuffer();
+        GlStateManager.enableBlend();
+        GlStateManager.disableTexture2D();
+        GlStateManager.tryBlendFuncSeparate(GlStateManager.SourceFactor.SRC_ALPHA, GlStateManager.DestFactor.ONE_MINUS_SRC_ALPHA, GlStateManager.SourceFactor.ONE, GlStateManager.DestFactor.ZERO);
+        RenderUtil.setColor(color);
+        GlStateManager.glLineWidth(width);
+        bufferbuilder.begin(GL11.GL_LINE_STRIP, DefaultVertexFormats.POSITION);
+        bufferbuilder.pos(x, y, 0.0D).endVertex();
+        bufferbuilder.pos(x2, y2, 0.0D).endVertex();
+        tessellator.draw();
+        GlStateManager.enableTexture2D();
+        GlStateManager.disableBlend();
+        GlStateManager.glLineWidth(1);
     }
 }
