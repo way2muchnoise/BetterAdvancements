6e3c878ed3762da07766ac74acd77b53a7840e1f
way2muchnoise <jens.baetens@live.be>
2021-05-20 19:42:21 +0200

    Mark mod as client only and not needed on the server
    long overdue fix for #93 and #96

--- src/main/java/betteradvancements/BetterAdvancements.java
+++ src/main/java/betteradvancements/BetterAdvancements.java
@@ -5,10 +5,13 @@ import betteradvancements.handler.GuiOpenHandler;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.DistExecutor;
+import net.minecraftforge.fml.ExtensionPoint;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;;
+import net.minecraftforge.fml.network.FMLNetworkConstants;
+import org.apache.commons.lang3.tuple.Pair;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -18,6 +21,8 @@ public class BetterAdvancements {
     public static final Logger log = LogManager.getLogger();
 
     public BetterAdvancements() {
+        //Make sure the mod being absent on the other network side does not cause the client to display the server as incompatible
+        ModLoadingContext.get().registerExtensionPoint(ExtensionPoint.DISPLAYTEST, () -> Pair.of(() -> FMLNetworkConstants.IGNORESERVERONLY, (a, b) -> true));
         DistExecutor.unsafeRunWhenOn(Dist.CLIENT, () -> () -> {
             ModLoadingContext.get().registerConfig(ModConfig.Type.CLIENT, Config.CLIENT);
             FMLJavaModLoadingContext.get().getModEventBus().register(Config.instance);
