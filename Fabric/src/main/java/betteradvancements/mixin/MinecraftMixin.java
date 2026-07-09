f9e5cc7a80eaf474ebfde7894c9ce2f4c963a046
way2muchnoise <jens.baetens@live.be>
2026-07-09 17:16:27 +0200

    update mixin for Fabric to correctly intercept Advancement screen

--- Fabric/src/main/java/betteradvancements/mixin/MinecraftMixin.java
+++ Fabric/src/main/java/betteradvancements/mixin/MinecraftMixin.java
@@ -2,19 +2,19 @@ package betteradvancements.mixin;
 
 import betteradvancements.common.gui.BetterAdvancementsScreen;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.Gui;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.client.gui.screens.advancements.AdvancementsScreen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
-@Mixin(Minecraft.class)
+@Mixin(Gui.class)
 public class MinecraftMixin {
-    @ModifyVariable(method = "setScreenAndShow", at = @At("HEAD"), ordinal = 0, argsOnly = true)
+    @ModifyVariable(method = "setScreen", at = @At("HEAD"), ordinal = 0, argsOnly = true)
     private Screen swapAdvancementsScreen(Screen screen) {
         if (screen instanceof AdvancementsScreen) {
-            Minecraft mc = Minecraft.getInstance();
-            return new BetterAdvancementsScreen(mc.player.connection.getAdvancements());
+            return new BetterAdvancementsScreen(Minecraft.getInstance().player.connection.getAdvancements());
         } else {
             return screen;
         }
