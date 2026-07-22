2ab155de7a29f52db24f316e8acf6538082f4a36
way2muchnoise <jens.baetens@live.be>
2026-07-22 15:06:25 +0200

    add translation guesses for armor trimming, fixes #243

--- Common/src/main/java/betteradvancements/common/util/CriterionTranslator.java
+++ Common/src/main/java/betteradvancements/common/util/CriterionTranslator.java
@@ -5,16 +5,26 @@ import net.minecraft.locale.Language;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.MutableComponent;
 
+import java.util.LinkedList;
 import java.util.List;
 
 public class CriterionTranslator {
     public static MutableComponent tryTranslateCriterion(AdvancementHolder advancementHolder, String criterion) {
-        String betterAdvancementsTranslation = "betteradvancements.criterion." + advancementHolder.id() + "." + criterion;
         String preppedCriterion = criterion.replace(":",".");
-        String biomeTranslation = "biome." + preppedCriterion;
-        String itemTranslation = "item." + preppedCriterion;
-        String blockTranslation = "block." + preppedCriterion;
-        List<String> translations = List.of(preppedCriterion, betterAdvancementsTranslation, biomeTranslation, itemTranslation, blockTranslation);
+        List<String> translations = new LinkedList<>();
+        translations.add(preppedCriterion);
+        translations.add("betteradvancements.criterion." + advancementHolder.id() + "." + criterion);
+        translations.add("biome." + preppedCriterion);
+        translations.add("item." + preppedCriterion);
+        translations.add("block." + preppedCriterion);
+        translations.add("entity." + preppedCriterion);
+
+        if (criterion.startsWith("armor_trimmed")) {
+            String guessType = criterion.split(":")[1].split("_")[0];
+            String guessOrigin = criterion.split(":")[0].replace("armor_trimmed","").split("_")[1];
+            translations.add("trim_pattern." + guessOrigin + "." + guessType);
+            translations.add("trim_material." + guessOrigin + "." + guessType);
+        }
 
         Language language = Language.getInstance();
         for (String translation : translations) {
