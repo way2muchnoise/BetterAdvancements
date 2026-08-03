1fd067b4de74f82b9767bda7c62ec023e534d550
way2muchnoise <jens.baetens@live.be>
2026-08-03 16:25:01 +0200

    fix Fabric Mixin for 26.1

--- Fabric/src/main/java/betteradvancements/mixin/MinecraftMixin.java
+++ Fabric/src/main/java/betteradvancements/mixin/MinecraftMixin.java
@@ -9,7 +9,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
-@Mixin(Gui.class)
+@Mixin(Minecraft.class)
 public class MinecraftMixin {
     @ModifyVariable(method = "setScreen", at = @At("HEAD"), ordinal = 0, argsOnly = true)
     private Screen swapAdvancementsScreen(Screen screen) {
